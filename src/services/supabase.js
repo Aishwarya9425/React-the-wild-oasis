566f0e6147a33787bb2a939afbffffe8d5ab7d3f
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-19 22:42:33 +0530

    exporting supabase url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://cipyjnxhnwnsqlyjwmnq.supabase.co";
+export const supabaseUrl = "https://cipyjnxhnwnsqlyjwmnq.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpcHlqbnhobnduc3FseWp3bW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUxMjU3NzIsImV4cCI6MjAxMDcwMTc3Mn0.krEvN72VcKs6lR1toEL0RLPJWjR77HGMZ3uBIAt6h9o";
 const supabase = createClient(supabaseUrl, supabaseKey);
