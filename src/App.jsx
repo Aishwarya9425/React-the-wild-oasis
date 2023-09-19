2cfb15f96e9702b7b1de685bc4f1554210f0fa76
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-09-19 17:13:53 +0530

    react router dom , BrowserRouter, routes

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
+          {/* child routes */}
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
